ce142908c78b21fb3bc707c7a8d3c9428e763274
BAI <153406428+BaisGit@users.noreply.github.com>
2024-08-19 22:04:03 +0800

    Update Prediction.py

--- Models/RandomForest/Prediction.py
+++ Models/RandomForest/Prediction.py
@@ -1,1 +1,26 @@
+import joblib
+import re
+
+# load model
+rf_clf = joblib.load('random_forest_model.pkl')
+vectorizer = joblib.load('tfidf_vectorizer.pkl')
+label_encoder = joblib.load('label_encoder.pkl')
+
+# user input
+input = input('please input....')
+custom_input = [f'''{input}''']
+
+# convert the input to string
+custom_input = [str(x) for x in custom_input]
+
+# pre-process data
+custom_input_transformed = vectorizer.transform(custom_input)
+
+# prediction
+custom_prediction = rf_clf.predict(custom_input_transformed)
+
+# convert the prediction to original label
+custom_prediction_label = label_encoder.inverse_transform(custom_prediction)
+
+print(f"Prediction: {custom_prediction_label}")
 
